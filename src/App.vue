f3f30a06f83ae2303d82c7688696cc7d428f00c2
therealrinku <arvy2032@gmail.com>
2021-10-02 08:20:46 +0545

    search field added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
-<template> </template>
+<template>
+  <h2>Dashboard</h2>
+  <input
+    class="search-input"
+    type="text"
+    v-model="searchQuery"
+    placeholder="Search for comment"
+  />
+</template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {},
 };
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 85vw;
+  margin: auto;
+}
+
+.search-input {
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  padding: 7px 10px 7px 7px;
+  width: 350px;
+  outline: none;
+  border-radius: 5px;
+}
+
+.search-input:focus {
+  box-shadow: 0 0 0 0.5pt green;
 }
 </style>
