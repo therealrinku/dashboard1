732649c55084115ded714b7def84c0501008f644
therealrinku <arvy2032@gmail.com>
2021-10-02 09:05:13 +0545

    select component and toolbar added

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,47 @@
     class="search-input"
     type="text"
     v-model="searchQuery"
-    placeholder="Search for comment"
+    placeholder="Search for Comment"
   />
+
+  <div class="toolbar">
+    <h4>Sorting and Filtering</h4>
+    <div class="toolbar-tools">
+      <Select
+        label="Filter by type"
+        :options="[
+          { id: 1, name: `Waitlist` },
+          { id: 2, name: `Student` },
+          { id: 3, name: `None` },
+        ]"
+      />
+      <span style="margin-left:5px;" />
+      <Select
+        label="Filter by postcode"
+        :options="[
+          { id: 1, name: 2000 },
+          { id: 2, name: 2001 },
+          { id: 3, name: `None` },
+        ]"
+      />
+      <span style="margin-left:5px;" />
+      <Select
+        label="Sort by"
+        :options="[
+          { id: 1, name: `Id` },
+          { id: 2, name: `Type` },
+        ]"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Select from "./components/Select.vue";
+
 export default {
   name: "App",
+  components: { Select },
   data() {
     return {
       searchQuery: "",
@@ -37,5 +71,16 @@ export default {
 
 .search-input:focus {
   box-shadow: 0 0 0 0.5pt green;
+}
+
+.toolbar {
+  margin-top: 35px;
+}
+
+.toolbar-tools {
+  display: flex;
+  align-items: center;
+  margin-top: -15px;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p style="font-size:13px;margin-bottom:3px;">{{ label }}</p>
+    <select>
+      <option :key="option.id" v-for="option in options">{{
+        option.name
+      }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Select",
+  props: {
+    label: String,
+    options: Array,
+  },
+};
+</script>
+
+<style scoped>
+select {
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  padding: 7px 10px 7px 7px;
+  width: 350px;
+  outline: none;
+  border-radius: 5px;
+  background: white;
+}
+</style>
