3d90f34de5bd50006566eebf0bd645dc5dd19eeb
therealrinku <arvy2032@gmail.com>
2021-10-02 13:56:54 +0545

    searching functionality completed

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,8 @@
   </div>
 
   <div class="data-table">
-    <h4>Enquiries</h4>
-    <Table :tableData="enquiries" />
+    <h4>Enquiries(showing {{ filteredEnquiries.length }} enquiries)</h4>
+    <Table :tableData="filteredEnquiries" />
   </div>
 </template>
 
@@ -48,152 +48,159 @@
 import Select from "./components/Select.vue";
 import Table from "./components/Table.vue";
 
+const enquiries = [
+  {
+    comments:
+      "The black-footed cat is the smallest wild cat, at just 20 inches  long.",
+    id: 39435,
+    postcode: 2000,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "Egg yolks are one of the few foods that naturally contain Vitamin  D.",
+    id: 39436,
+    postcode: 2001,
+    type: "Waitlist",
+  },
+  {
+    comments: "Peaches are the third most popular fruit grown in America.",
+    id: 39437,
+    postcode: 2000,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "The fear of cooking is known as Mageirocophobia and is a  recognized phobia.",
+    id: 39438,
+    postcode: 2001,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "1912 saw the last Olympic gold medals made entirely out of  gold.",
+    id: 3939,
+    postcode: 2000,
+    type: "Waitlist",
+  },
+  {
+    comments: "The only letter that doesn’t appear on the periodic table is J.",
+    id: 39440,
+    postcode: 2001,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "A dog was the first living creature to be sent into space in  1957.",
+    id: 39441,
+    postcode: 2000,
+    type: "Waitlist",
+  },
+  {
+    comments: "Clinomania is the excessive desire to lay in bed all day.",
+    id: 39442,
+    postcode: 2001,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "Red pandas have no living relatives – they aren’t even related to  giant pandas.",
+    id: 39443,
+    postcode: 2000,
+    type: "Waitlist",
+  },
+  {
+    comments:
+      "After Christianity, the largest religious affiliation in the U.S.  is Judaism.",
+    id: 39444,
+    postcode: 2001,
+    type: "Waitlist",
+  },
+  {
+    comments: "Dogs have a total of 42 teeth when they are fully-grown.",
+    id: 39445,
+    postcode: 2001,
+    type: "Student",
+  },
+  {
+    comments: "The center of a comet is called a nucleus.",
+    id: 39446,
+    postcode: 2000,
+    type: "Student",
+  },
+  {
+    comments:
+      "The Earth is the only planet in our solar system not named after  either a Greek or Roman God.",
+    id: 39447,
+    postcode: 2001,
+    type: "Student",
+  },
+  {
+    comments: "26 is the atomic number of iron.",
+    id: 39448,
+    postcode: 2000,
+    type: "Student",
+  },
+  {
+    comments: "If you keep a goldfish in a dark room, it will become pale.",
+    id: 39449,
+    postcode: 2001,
+    type: "Student",
+  },
+  {
+    comments:
+      "The patterns on a giraffe are totally unique – no two giraffes  are ever the same.",
+    id: 39450,
+    postcode: 2000,
+    type: "Student",
+  },
+  {
+    comments: "A standard 12oz jar of honey take 864 bees to produce.",
+    id: 39451,
+    postcode: 2001,
+    type: "Student",
+  },
+  {
+    comments:
+      "The first movie ever to put out a motion-picture soundtrack was  Snow White and the Seven Dwarves.",
+    id: 39452,
+    postcode: 2000,
+    type: "Student",
+  },
+  {
+    comments:
+      "In general, people tend to read as much as 10% slower from a  screen than from paper.",
+    id: 39453,
+    postcode: 2001,
+    type: "Student",
+  },
+  {
+    comments:
+      "The world’s longest place name has 85 letters & is located in New  Zealand.",
+    id: 39454,
+    postcode: 2000,
+    type: "Student",
+  },
+];
+
 export default {
   name: "App",
   components: { Select, Table },
   data() {
     return {
       searchQuery: "",
-      enquiries: [
-        {
-          comments:
-            "The black-footed cat is the smallest wild cat, at just 20 inches  long.",
-          id: 39435,
-          postcode: 2000,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "Egg yolks are one of the few foods that naturally contain Vitamin  D.",
-          id: 39436,
-          postcode: 2001,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "Peaches are the third most popular fruit grown in America.",
-          id: 39437,
-          postcode: 2000,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "The fear of cooking is known as Mageirocophobia and is a  recognized phobia.",
-          id: 39438,
-          postcode: 2001,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "1912 saw the last Olympic gold medals made entirely out of  gold.",
-          id: 3939,
-          postcode: 2000,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "The only letter that doesn’t appear on the periodic table is J.",
-          id: 39440,
-          postcode: 2001,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "A dog was the first living creature to be sent into space in  1957.",
-          id: 39441,
-          postcode: 2000,
-          type: "Waitlist",
-        },
-        {
-          comments: "Clinomania is the excessive desire to lay in bed all day.",
-          id: 39442,
-          postcode: 2001,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "Red pandas have no living relatives – they aren’t even related to  giant pandas.",
-          id: 39443,
-          postcode: 2000,
-          type: "Waitlist",
-        },
-        {
-          comments:
-            "After Christianity, the largest religious affiliation in the U.S.  is Judaism.",
-          id: 39444,
-          postcode: 2001,
-          type: "Waitlist",
-        },
-        {
-          comments: "Dogs have a total of 42 teeth when they are fully-grown.",
-          id: 39445,
-          postcode: 2001,
-          type: "Student",
-        },
-        {
-          comments: "The center of a comet is called a nucleus.",
-          id: 39446,
-          postcode: 2000,
-          type: "Student",
-        },
-        {
-          comments:
-            "The Earth is the only planet in our solar system not named after  either a Greek or Roman God.",
-          id: 39447,
-          postcode: 2001,
-          type: "Student",
-        },
-        {
-          comments: "26 is the atomic number of iron.",
-          id: 39448,
-          postcode: 2000,
-          type: "Student",
-        },
-        {
-          comments:
-            "If you keep a goldfish in a dark room, it will become pale.",
-          id: 39449,
-          postcode: 2001,
-          type: "Student",
-        },
-        {
-          comments:
-            "The patterns on a giraffe are totally unique – no two giraffes  are ever the same.",
-          id: 39450,
-          postcode: 2000,
-          type: "Student",
-        },
-        {
-          comments: "A standard 12oz jar of honey take 864 bees to produce.",
-          id: 39451,
-          postcode: 2001,
-          type: "Student",
-        },
-        {
-          comments:
-            "The first movie ever to put out a motion-picture soundtrack was  Snow White and the Seven Dwarves.",
-          id: 39452,
-          postcode: 2000,
-          type: "Student",
-        },
-        {
-          comments:
-            "In general, people tend to read as much as 10% slower from a  screen than from paper.",
-          id: 39453,
-          postcode: 2001,
-          type: "Student",
-        },
-        {
-          comments:
-            "The world’s longest place name has 85 letters & is located in New  Zealand.",
-          id: 39454,
-          postcode: 2000,
-          type: "Student",
-        },
-      ],
+      filteredEnquiries: enquiries,
     };
   },
-  methods: {},
+  watch: {
+    searchQuery: function filterEnquiries() {
+      this.filteredEnquiries = enquiries.filter((enq) =>
+        enq.comments
+          .toLowerCase()
+          .includes(this.searchQuery.trim().toLowerCase())
+      );
+    },
+  },
 };
 </script>
 
